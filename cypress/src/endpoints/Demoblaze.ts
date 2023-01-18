d3ec261ec893ecc3ec2e12474e2850efda2e637a
Vadym Nastoiashchyi <Vadim.89@ukr.net>
2023-01-18 13:36:50 +0200

    Refactored Demoblaze.ts

--- cypress/src/endpoints/Demoblaze.ts
+++ cypress/src/endpoints/Demoblaze.ts
@@ -1,5 +1,7 @@
 class Demoblaze {
+    // https://api.demoblaze.com/
     private static readonly serviceRoot = 'https://api.demoblaze.com/';
+    // https://api.demoblaze.com/bycat
     public static readonly bycat = `${Demoblaze.serviceRoot}bycat`;
 }
 
