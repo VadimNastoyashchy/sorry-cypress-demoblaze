2652176d51df292722e8499b358ed5cfd1b802f0
Vadym Nastoiashchyi <Vadim.89@ukr.net>
2023-01-18 10:01:46 +0200

    Deleted unused components in HomePage.ts

--- cypress/src/pages/HomePage.ts
+++ cypress/src/pages/HomePage.ts
@@ -1,21 +1,13 @@
 import Header from '../components/Header';
-import LogInModal from '../modals/LogInModal';
 import BasePage from '../base/BasePage';
 import Categories from '../components/Categories';
 import Carousel from '../components/slider/Carousel';
-import ContactModal from '../modals/ContactModal';
-import AboutUsModal from '../modals/AboutUsModal';
-import SignUpModal from '../modals/SignUpModal';
 
 class HomePage extends BasePage {
 
     public header: typeof Header = Header;
-    public logInModal: typeof LogInModal = LogInModal;
     public categories: typeof Categories = Categories;
     public carousel: typeof Carousel = Carousel;
-    public contactModal: typeof ContactModal = ContactModal;
-    public aboutUsModal: typeof AboutUsModal = AboutUsModal;
-    public singUpModal: typeof SignUpModal = SignUpModal;
 
     constructor() {
         super('index.html');
